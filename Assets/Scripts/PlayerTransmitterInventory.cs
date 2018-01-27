5dfc3ba1d51b87fa7cf99ab5650b703f8f120d5d
Avi Miller <TrebecS@Gmail.com>
2018-01-27 13:30:37 -0800

    Added debug script for transmitters and player inventory

--- Assets/Scripts/PlayerTransmitterInventory.cs
+++ Assets/Scripts/PlayerTransmitterInventory.cs
@@ -4,13 +4,100 @@ using UnityEngine;
 
 public class PlayerTransmitterInventory : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	[SerializeField] KeyCode activateButton = KeyCode.X;
+	[SerializeField] GameObject transmitterPrefab;
+	[SerializeField] Transform transmitterDropPoint;
+	[SerializeField] Transform pickupCenter;
+	[SerializeField] int transmitterCount;
+	[SerializeField] float pickupRadius;
+
+	float distance;
+	Transmitter nearest = null;
 	
 	// Update is called once per frame
 	void Update () {
-		
+		TransmitterHighlight();
+		UserControl();
 	}
+
+	//------------------------------------------------------------
+	// updates the status of the nearest transmitter
+	//------------------------------------------------------------
+	void TransmitterHighlight(){
+		float newDistance;
+		Transmitter newNearest = FindNearestTransmitter(out newDistance);
+
+		if (newNearest != null && newDistance <= pickupRadius){
+			//if(newNearest != nearest)
+			//use this to determine whether to create, destroy or move any selection effect
+		}
+
+		nearest = newNearest;
+		distance = newDistance;
+	}
+		
+	//------------------------------------------------------------
+	// user interaction for this mechanic
+	//------------------------------------------------------------
+	void UserControl(){
+		if (Input.GetKeyDown(activateButton)){
+			TransmitterInteract();
+		}
+	}
+
+	//------------------------------------------------------------
+	// determines whether the player is able to pick up a transmitter
+	// if not, the player will try to place one instead
+	//------------------------------------------------------------
+	void TransmitterInteract(){
+		if (nearest != null && distance <= pickupRadius)
+			PickupTransmitter(nearest);
+		else if (transmitterCount > 0)
+			PlaceTransmitter();
+	}
+
+	//------------------------------------------------------------
+	// finds the nearest transmitter THAT IS NOT THE START ONE
+	//------------------------------------------------------------
+	Transmitter FindNearestTransmitter(out float distance){
+		Transmitter[] emitters = GameObject.FindObjectsOfType<Transmitter>();
+
+		Transmitter nearest = null;
+		distance = Mathf.Infinity;
+
+		foreach (Transmitter t in emitters){
+			if (t.IsStartTransmitter())
+				continue;
+
+			float currentDistance = Vector3.Distance(t.transform.position, pickupCenter.position);
+
+			if (nearest == null){
+				nearest = t;
+				distance = currentDistance;
+			} else if(currentDistance < distance){
+				nearest = t;
+				distance = currentDistance;
+			}
+		}
+
+		return nearest;
+	}
+
+	//------------------------------------------------------------
+	// destroys a selected transmitter and increments the player inventory count
+	//------------------------------------------------------------
+	void PickupTransmitter(Transmitter t){
+		Destroy(t.gameObject);
+		++transmitterCount;
+	}
+
+	//------------------------------------------------------------
+	// creates a transmitter and decrements the player inventory
+	//------------------------------------------------------------
+	void PlaceTransmitter(){
+		--transmitterCount;
+		GameObject.Instantiate(transmitterPrefab, transmitterDropPoint.position, transform.rotation);
+	}
+
+
 }
