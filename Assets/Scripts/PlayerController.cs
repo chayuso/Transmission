3a80378719a4ef99b44239b08c92269eaa0c1d5a
Christopher Ayuso <chayuso14@gmail.com>
2018-01-27 14:03:04 -0800

    Custom Transmitter

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject TransmitterPrefab;
+    public GameObject PlayerCharacter;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            SpawnObject(PlayerCharacter.transform.position, PlayerCharacter.transform.rotation, TransmitterPrefab);
+        }
+    }
+    void SpawnObject(Vector3 Position, Quaternion Rotation, GameObject Prefab)
+    {
+        var transmitter = (GameObject)Instantiate(
+            Prefab,
+            Position,
+            Rotation);
+    }
+
+}
