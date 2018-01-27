b000a62a553abd2bb4ee52609654d9c273dffe16
Christopher Ayuso <chayuso14@gmail.com>
2018-01-27 13:23:18 -0800

    AIMovement

---
+++ Assets/Scripts/ToggleSwitch.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSwitch : MonoBehaviour {
+    public GameObject Button;
+    public Material InactiveMaterial;
+    public Material ActiveMaterial;
+    public GameObject Wall;
+    Vector3 initButtonPos;
+    bool isStepped = false;
+    public bool manualEnable = false;
+	// Use this for initialization
+	void Start () {
+        initButtonPos = Button.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (manualEnable)
+        {
+            Wall.SetActive(manualEnable);
+            if (manualEnable)
+            {
+                Button.transform.localPosition = new Vector3(initButtonPos.x, .05f, initButtonPos.z);
+                Button.GetComponent<Renderer>().material = ActiveMaterial;
+
+            }
+        }
+        else
+        {
+            Wall.SetActive(isStepped);
+            if (isStepped)
+            {
+                Button.transform.localPosition = new Vector3(initButtonPos.x, .05f, initButtonPos.z);
+                Button.GetComponent<Renderer>().material = ActiveMaterial;
+
+            }
+            else
+            {
+                Button.transform.localPosition = initButtonPos;
+                Button.GetComponent<Renderer>().material = InactiveMaterial;
+                Wall.GetComponent<WallScript>().ReEnableEnemies();
+            }
+        }
+
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "ThirdPersonController")
+        {
+            isStepped = true;
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.name == "ThirdPersonController")
+        {
+            isStepped = false;
+        }
+    }
+}
---
+++ Assets/Scripts/WallScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour {
+    public List<GameObject> DisabledEnemies;
+	// Use this for initialization
+	void Start () {
+        print("testc");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "AIThirdPersonController")
+        {
+            col.gameObject.GetComponent<AIController>().disableMovement = true;
+            DisabledEnemies.Add(col.gameObject);
+        }
+    }
+    public void ReEnableEnemies()
+    {
+        for (int i = 0; i<DisabledEnemies.Count;i++)
+        {
+            DisabledEnemies[i].GetComponent<AIController>().disableMovement = false;
+        }
+        DisabledEnemies.Clear();
+    }
+
+}
