2245a3a35325b46a54cdbe1724bba4e03f6d75b1
Conner K Ward <conner.k.ward@gmail.com>
2018-01-27 23:21:19 -0800

    public

--- Assets/Scripts/ToggleSwitch.cs
+++ Assets/Scripts/ToggleSwitch.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ToggleSwitch : MonoBehaviour {
-    private GameObject Button;
+    public GameObject Button;
     public Material InactiveMaterial;
     public Material ActiveMaterial;
     public GameObject Wall;
