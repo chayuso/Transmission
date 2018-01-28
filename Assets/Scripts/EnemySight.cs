4a71523f12025b408d2f495b6cb0d8546c827431
Avi Miller <TrebecS@Gmail.com>
2018-01-27 22:26:56 -0800

    Added default sight range for enemies and changed hunting parameters for transmitters

--- Assets/Scripts/EnemySight.cs
+++ Assets/Scripts/EnemySight.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemySight : MonoBehaviour {
 
-	[SerializeField] float sightRange;
+	[SerializeField] float sightRange = 5;
 
 	// Use this for initialization
 	void Start () {
