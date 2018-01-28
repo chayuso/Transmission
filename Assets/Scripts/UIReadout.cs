a6a85206416736f80c30b0efb8e89642cac1e587
Avi Miller <TrebecS@Gmail.com>
2018-01-28 14:39:34 -0800

    Added UI

---
+++ Assets/Scripts/UIReadout.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIReadout : MonoBehaviour {
+
+	[SerializeField] Text transmitterText;
+	[SerializeField] Text houseText;
+
+	GameState gameStateObj;
+
+	// Use this for initialization
+	void Start () {
+		gameStateObj = FindObjectOfType<GameState>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transmitterText.text = "Transmitters Remaining\n" + (gameStateObj.transmitterLimit - gameStateObj.transmittersDeployed);
+		houseText.text = "Nodes Powered\n" + gameStateObj.litHousesCount + "/" + gameStateObj.winCount;
+	}
+}
